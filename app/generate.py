5bcc1ec9765990be86a2bd60ac940083c817153d
Vladislav <vladislav.mnac@gmail.com>
2025-05-11 19:44:12 +0300

    handlers.py

--- app/generate.py
+++ app/generate.py
@@ -1,9 +1,11 @@
 from openai import AsyncOpenAI
 from config import API_KEY, API_URL
+import base64
 
 client = AsyncOpenAI(api_key=API_KEY, base_url=API_URL)
 
-async def ai_generate(text: str):
+async def ai_generate(text: str, image_path: str = None):
+    default_text = '. Cделай ТОЛЬКО json текст из данной картинки, ничего добавлять к ответу не нужно'
     response = await client.chat.completions.create(
         model="mistralai/mistral-small-3.1-24b-instruct:free",
         messages=[
@@ -12,7 +14,7 @@ async def ai_generate(text: str):
                 "content": [
                     {
                         "type": "text",
-                        "text": text
+                        "text": (text + default_text) if text else default_text
                     },
                     {
                         "type": "image_url",
--- app/handlers.py
+++ app/handlers.py
@@ -1,3 +1,5 @@
+import os
+import tempfile
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
@@ -11,10 +13,36 @@ router = Router()
 
 class Gen(StatesGroup):
     wait = State()
+    pic_wait = State()
+
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer('Бот работает')
+
+@router.message(Command('pic'))
+async def cmd_pic(message: Message, state: FSMContext):
+    await message.answer('Отправьте, пожалуйста, картинку')
+    await state.set_state(Gen.pic_wait)
+
+@router.message(Gen.pic_wait, F.photo)
+async def handle_photo(message: Message, state: FSMContext):
+    await state.set_state(Gen.wait)
+
+    photo = message.photo[-1]
+    file = await message.bot.get_file(photo.file_id)
+
+    with tempfile.NamedTemporaryFile(delete_on_close=False, suffix='.jpg') as temp_file:
+        await message.bot.download_file(file.file_path, temp_file.name)
+        temp_file_path = temp_file.name
+
+    try:
+        response = await ai_generate("", temp_file_path)
+        await message.answer(response, parse_mode='Markdown')
+    finally:
+        os.unlink(temp_file_path)
+
+    await state.clear()
 
 @router.message(Gen.wait)
 async def stop_flood(message: Message):
