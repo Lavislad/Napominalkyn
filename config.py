e40bb6e96bd8cbf7026ae9ec2bc00c1c10235483
Vladislav <vladislav.mnac@gmail.com>
2025-05-11 21:51:37 +0300

    gitignore test

---
+++ config.py
@@ -0,0 +1,10 @@
+# API
+API_KEY = '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'
+API_URL = 'https://openrouter.ai/api/v1'
+
+# Telegram bot token
+BOT_TOKEN = '8142165825:AAFXZcr5UDo1IB5Auit9wzv8_VV_1-HWGBE'
+
+#DeepSeek
+# API_KEY = 'sk-52a740f9066f4fbda090281620cfe975'
+# API_URL = 'https://api.deepseek.com'
